872fd30c98b21c3509e01f7d785cb1bb9ba0eb74
MichaelRomeroSG <michael@socialgoodsoftware.com>
2023-11-02 19:01:08 -0500

    Update git ignore file

--- src/routes/request.sh
+++ src/routes/request.sh
@@ -1,3 +1,4 @@
 curl -X POST "https://accounts.spotify.com/api/token" \
      -H "Content-Type: application/x-www-form-urlencoded" \
      -d "grant_type=client_credentials&client_id=08bbb4f80f4140f28b749cd961cf97ba&client_secret=9e2cdcff9f6149cf8ffed808c824ccc5"
+
